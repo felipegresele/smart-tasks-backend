71b1570631a0f74d89d84ee0d7b86e3c7c6f7c3f
felipegresele <felipehgresele@gmail.com>
2026-04-22 15:06:49 -0300

    appConfig corrigido

--- src/main/java/com/task/task/config/AppConfig.java
+++ src/main/java/com/task/task/config/AppConfig.java
@@ -3,12 +3,17 @@ package com.task.task.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class AppConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
     }
 }
